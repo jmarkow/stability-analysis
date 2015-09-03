0444c2d4fd086d5c8cae6fa16d10aa1389dbb8a5
jmarkow <markowitzmeister@gmail.com>
2015-09-03 16:57:46 -0400

    Finalize ca2+ plots

--- stan_cadata_schnitzplot.m
+++ stan_cadata_schnitzplot.m
@@ -10,50 +10,42 @@ load(fullfile(dirs.agg_dir,dirs.fig_dir,'AwesomeNess.mat'));
 
 newdata=stan_cadata_format(data1,data2,data3,data4,data5);
 
-fig.localnorm=figure();
-stan_cadata_sortmat(newdata,'scaling','l','sort_day',1,'fig_nrows',3,'fig_row',1,'chk_day',1);
-stan_cadata_sortmat(newdata,'scaling','l','sort_day',3,'fig_nrows',3,'fig_row',2,'chk_day',1);
-stan_cadata_sortmat(newdata,'scaling','l','sort_day',5,'fig_nrows',3,'fig_row',3,'chk_day',1);
+fig.sortnorm=figure();
+stan_cadata_sortmat(newdata,'scaling','s','sort_day',1,'smoothing',.15,'smooth_kernel','g',...
+	'dff_check',.5,'padding',.6);
 ax=findall(gcf,'type','axes')
-linkaxes(ax,'x')
-xlim([1 2.4])
-title(ax(end-2),'Within day and roi scaling');
-ylabel(ax(end),'Day 1');
-ylabel(ax(end-5),'Day 3');
-ylabel(ax(end-10),'Day 5');
-set(ax(1),'xtick',xlim(),'TickLength',[0 0]);
-xlabel(ax(1),'Time (s)');
+linkaxes(ax,'xy')
+xlim([1.4 3])
+ylim([4.5 max(ylim())]);
+ylimits=ylim();
+set(ax(end),'YTick',ylim(),'YTickLabel',[ylim()-min(ylim())]+1,'FontSize',8);
+yh=ylabel(ax(end),'Cell');
+set(yh,'position',get(yh,'position')+[.3 0 0]);
+xlimits=xlim();
+h=line([xlimits(1) xlimits(1)+.4],[ylimits(2)+3 ylimits(2)+3],'color','k','parent',ax(1))
+set(h,'clipping','off');
+title(ax(end-2),'Sort day scaling');
+
 
 fig.roinorm=figure();
-stan_cadata_sortmat(newdata,'scaling','r','sort_day',1,'fig_nrows',3,'fig_row',1,'chk_day',1);
-stan_cadata_sortmat(newdata,'scaling','r','sort_day',3,'fig_nrows',3,'fig_row',2,'chk_day',1);
-stan_cadata_sortmat(newdata,'scaling','r','sort_day',5,'fig_nrows',3,'fig_row',3,'chk_day',1);
+stan_cadata_sortmat(newdata,'scaling','r','sort_day',1,'smoothing',.15,'smooth_kernel','g',...
+	'dff_check',.5,'padding',.6);
 ax=findall(gcf,'type','axes')
-linkaxes(ax,'x')
-xlim([1 2.4])
-title(ax(end-2),'Within roi across day scaling');
-ylabel(ax(end),'Day 1');
-ylabel(ax(end-5),'Day 3');
-ylabel(ax(end-10),'Day 5');
-set(ax(1),'xtick',xlim(),'TickLength',[0 0]);
-xlabel(ax(1),'Time (s)');
-
-fig.sortnorm=figure();
-stan_cadata_sortmat(newdata,'scaling','s','sort_day',1,'fig_nrows',3,'fig_row',1,'chk_day',1);
-stan_cadata_sortmat(newdata,'scaling','s','sort_day',3,'fig_nrows',3,'fig_row',2,'chk_day',1);
-stan_cadata_sortmat(newdata,'scaling','s','sort_day',5,'fig_nrows',3,'fig_row',3,'chk_day',1);
-ax=findall(gcf,'type','axes')
-linkaxes(ax,'x')
-xlim([1 2.4])
-title(ax(end-2),'Within roi sort day');
-ylabel(ax(end),'Day 1');
-ylabel(ax(end-5),'Day 3');
-ylabel(ax(end-10),'Day 5');
-set(ax(1),'xtick',xlim(),'TickLength',[0 0]);
-xlabel(ax(1),'Time (s)');
+linkaxes(ax,'xy')
+xlim([1.4 3])
+ylim([4.5 max(ylim())]);
+ylimits=ylim();
+set(ax(end),'YTick',ylim(),'YTickLabel',[ylim()-min(ylim())]+1,'FontSize',8);
+yh=ylabel(ax(end),'Cell');
+set(yh,'position',get(yh,'position')+[.3 0 0]);
+xlimits=xlim();
+h=line([xlimits(1) xlimits(1)+.4],[ylimits(2)+3 ylimits(2)+3],'color','k','parent',ax(1))
+set(h,'clipping','off');
+title(ax(end-2),'Across day scaling');
 
 names=fieldnames(fig);
 
 for i=1:length(names)
+	set(fig.(names{i}),'units','centimeters','position',[10 10 16 5],'paperpositionmode','auto');
 	markolab_multi_fig_save(fig.(names{i}),fullfile(dirs.agg_dir,dirs.fig_dir),[ 'schnitzplot_' names{i} ],'eps,png,fig,pdf');
 end
--- stan_cadata_sortmat.m
+++ stan_cadata_sortmat.m
@@ -11,14 +11,18 @@ movie_fs=22; % sampling rate of camera
 upsample=10; % upsample factor (set to 1 for no upsampling)
 upsample_method='spline'; % upsample method (spline and linear work fine)
 sort_day=3; % day to use for sorting
-peak_check=0; % check for peak consistency (not working yet)
+peak_check=0; % check for peak consistency 
+peak_thresh=.05; % if closest peak is >peak_thresh, exclude roi
 dff_check=.5; % check for dff peak 
 chk_day=1; % check for dff peak day
 scaling='r'; % scaling ('r' for within roi across days, 's' for within roi sort day, 'l' for within roi and day)
 smoothing=0; % smooth ca trace (not working yet)
+smooth_kernel='g'; % gauss smoothing kernel (b for boxcar)
 fig_row=1; % subplot row 
 fig_nrows=1; % total number of subplot rows
-padding=.8; % padding before and after song
+padding=1; % padding before and after song
+bin_fluo=0; % discretize fluorescence 
+nbins=10; % number of bins for discretization
 
 nparams=length(varargin);
 
@@ -36,6 +40,8 @@ for i=1:2:nparams
 			sort_day=varargin{i+1};
 		case 'peak_check'
 			peak_check=varargin{i+1};
+		case 'peak_thresh'
+			peak_thresh=varargin{i+1};
 		case 'dff_check'
 			dff_check=varargin{i+1};
 		case 'scaling'
@@ -48,6 +54,12 @@ for i=1:2:nparams
 			fig_nrows=varargin{i+1};
 		case 'chk_day'
 			chk_day=varargin{i+1};
+		case 'bin_fluo'
+			bin_fluo=varargin{i+1};
+		case 'nbins'
+			nbins=varargin{i+1};
+        case 'padding'
+            padding=varargin{i+1};
 	end
 end
 
@@ -61,18 +73,52 @@ ndays=length(DATA);
 [nsamples,nrois,ntrials]=size(DATA{1});
 
 if peak_check
+	exclude=[];
 	sortca.peaks=cell(1,ntrials);
 	sortca.mergevals=cell(1,ntrials);
 
+	% requires that the fb toolbox is in your PATH
+
 	for i=1:ntrials
-		[sortca.peaks{i},sortca.vals{i}]=fb_compute_peak_simple(DATA{1}(:,:,i),...
+		[sortca.peaks{i},sortca.vals{i}]=fb_compute_peak_simple(DATA{chk_day}(:,:,i),...
 			'thresh_t',.2,'debug',0,'onset_only',0,'thresh_hi',1,'thresh_int',8,'thresh_dist',.2); % thresh_int previously 5
 	end
 
 	% now flip through criteria to include
-	%
-	% inc_rois= etc. etc.
-	%
+	% get timing distance relative to first trial, exclude inconsistent rois
+
+	for i=1:nrois
+
+		% concatenate times, exclude double peaks
+	
+		template=sortca.peaks{1}{i};
+
+		if	isempty(template)
+			exclude(end+1)=i;
+			continue;
+		end
+
+		% otherwise, check other times
+	
+		for j=1:ntrials
+			mindist=inf;
+			for k=1:length(template)
+				tmp=min(abs(template(k)-sortca.peaks{j}{i}))/movie_fs;
+				if tmp<mindist
+					mindist=tmp;
+				end			
+			end
+		end
+
+		if mindist>peak_thresh
+			exclude(end+1)=i;
+		end
+
+	end
+
+	exclude
+	inc_rois=setdiff(1:nrois,exclude);
+
 else
 
 	inc_rois=1:nrois;
@@ -80,6 +126,45 @@ else
 end
 
 ave_mat=cell(1,ndays);
+
+if smoothing>0
+
+	ts=round(smoothing*movie_fs);
+	
+	if strcmp(smooth_kernel,'b')	
+		kernel=ones(ts,1)/ts;
+	elseif strcmp(smooth_kernel,'g')
+		
+		kernx=[-3*smoothing:1/movie_fs:3*smoothing];
+		kernel=normpdf(kernx,0,smoothing);
+
+		%kernel=kernel./sum(kernel);
+	end
+
+	for i=1:ndays
+
+		% boxcar smoothing, swap out kernel here (gauss more appropriate?)
+		% zero pad or repeat extreme values (or wrap)
+
+		disp(['Note causal filter length ' num2str(length(kernel))]);
+		
+		tmp=DATA{i};
+
+		% wrap around for smoothing
+	
+		zeropad_len=round(length(kernel)/2);
+		%zeropad=repmat(DATA{i}(1,:,:),[zeropad_len 1 1]);
+		
+		zeropad=DATA{i}(end-(zeropad_len-1):end,:,:);
+
+		tmp=[zeropad;tmp];
+		
+		tmp=filter(kernel,1,tmp);
+		DATA{i}=tmp(zeropad_len+1:end,:,:);
+
+	end
+
+end
 
 for i=1:ndays
 	DATA{i}=DATA{i}(:,inc_rois,:);
@@ -180,8 +265,12 @@ end
 
 % get peak locations, anything in the pads is removed
 
+if smoothing
+	phase_shift=round(length(kernel)/2);
+end
+
 [~,peakloc]=max(ave_mat{sort_day});
-del=(peakloc<pad_smps|peakloc>nsamples-pad_smps);
+del=(peakloc<pad_smps|peakloc>nsamples-(pad_smps-phase_shift));
 
 for i=1:ndays
 	ave_mat{i}(:,del)=[];
@@ -194,10 +283,23 @@ end
 
 % plot
 
+if bin_fluo
+	bin_edges=0:1/nbins:1
+	for i=1:ndays
+		[~,ave_mat{i}]=histc(ave_mat{i},bin_edges);
+		ave_mat{i}
+	end
+end
+
 for i=1:ndays
 	ax(i)=subplot(fig_nrows,ndays,(fig_row-1)*ndays+i);
 	imagesc(interp_x,[],ave_mat{i}(:,idx)');
-	caxis([0 1]);
+
+	if bin_fluo
+		caxis([0 nbins+1]);
+	else
+		caxis([0 1]);
+	end
 	set(gca,'xtick',[],'ytick',[]);
 end
 
