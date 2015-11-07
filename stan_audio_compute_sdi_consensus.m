d2b7a215a0bb31f62deb8d5ed9a0ba8d56b4874d
Jeff Markowitz <markowitzmeister@gmail.com>
2015-11-07 15:20:47 -0500

    Made sdi_consensus more efficient

--- stan_audio_compute_sdi_consensus.m
+++ stan_audio_compute_sdi_consensus.m
@@ -9,20 +9,31 @@
 cur_dir=fullfile(dirs.agg_dir,dirs.sdi_dir,'use_data');
 listing=dir(fullfile(cur_dir,'*.mat'));
 
+% get dims
+
 for i=1:length(listing)
 
 	disp([listing(i).name]);
 	load(fullfile(cur_dir,listing(i).name),'agg_audio');
 
+	[consensus f t]=acontour(agg_audio.data(:,1),agg_audio.fs);
 	% compute sdi, relevant quantities, store
 
-	for j=1:size(agg_audio.data,2)
-		disp([num2str(j) ' of ' num2str(size(agg_audio.data,2))]);
-		[data(i).consensus{j} data(i).f{j} data(i).t{j} data(i).auditory_contours{j} data(i).sonogram{j}]=...
-			acontour(agg_audio.data(:,j),agg_audio.fs);
+	ntrials=size(agg_audio.data,2);
+	[rows,columns]=size(consensus);
+
+	consensus=zeros(rows,columns,ntrials,'single');
+
+	parfor j=1:ntrials
+		disp([num2str(j) ' of ' num2str(ntrials)]);
+		[consensus(:,:,j)]=acontour(agg_audio.data(:,j),agg_audio.fs);
 	end
 
-	clear agg_audio;
+	data(i).consensus=consensus;
+	data(i).f=f;
+	data(i).t=t;
+
+	clearvars consensus f t agg_audio;
 
 end
 
