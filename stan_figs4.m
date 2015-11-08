3e7cc33c978bbb57a33b6670c7c949c16752e480
Jeff Markowitz <markowitzmeister@gmail.com>
2015-11-08 11:52:41 -0500

    Consensus plotting

--- stan_figs4.m
+++ stan_figs4.m
@@ -2,9 +2,16 @@
 %
 %
 
+consensus=1;
+threshold=5;
+
 [options,dirs]=stan_preflight;
 
-load(fullfile(dirs.agg_dir,dirs.sdi_dir,'sdi_data.mat'),'data');
+if consensus
+  load(fullfile(dirs.agg_dir,dirs.sdi_dir,'sdi_data_consensus.mat'),'data');
+else
+  load(fullfile(dirs.agg_dir,dirs.sdi_dir,'sdi_data.mat'),'data');
+end
 load custom_colormaps;
 
 %%%%% plot both, use nice colormap, etc. etc.
@@ -12,14 +19,24 @@ ax=[];
 
 fig.sdi=figure();
 ax(1)=subplot(2,1,1);
-imagesc(data(1).t,data(1).f/1e3,data(1).sdi.im);
+
+if consensus
+  imagesc(data(1).t,data(1).f/1e3,mean(data(1).sdi.consensus>threshold,3));
+else
+  imagesc(data(1).t,data(1).f/1e3,data(1).sdi.im);
+end
+
 axis xy;
 %colormap(tim_map2);
 %caxis([0 .4]);
 colorbar();
 
 ax(2)=subplot(2,1,2);
-imagesc(data(2).t,data(2).f/1e3,data(2).sdi.im);
+if consensus
+  imagesc(data(2).t,data(2).f/1e3,mean(data(2).sdi.consensus>threshold,3));
+else
+  imagesc(data(2).t,data(2).f/1e3,data(2).sdi.im);
+end
 axis xy;
 %colormap(tim_map2);
 %caxis([0 .4]);
